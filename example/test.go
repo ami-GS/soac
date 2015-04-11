6563b73c10b177e79f2003d0a3cefa7e82efc52c
ami-GS <1991.daiki@gmail.com>
2015-04-11 18:56:03 +0900

    update

--- example/test.go
+++ example/test.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	c := soac.NewChanger()
-	fmt.Printf("%s\n", c.Set(1).Underline().White().Out())
+	fmt.Printf("int out = %s\n", c.Underline().White().Apply(123456))
 	c.Reset()
-	fmt.Printf("%s\n", c.Set(1).Out())
-	fmt.Printf("%s\n", c.Set("aiueoKAKIKU!!").Yellow().Underline().Bblue().Out())
-	c.Reset()
-	fmt.Printf("%s\n", c.Set("aiueoKAKIKU!!").Underline().Yellow().Out())
+	fmt.Printf("%s is %s\n", c.Bblue().Red().Bold().Apply("This"), c.Apply("continuing!!"))
+	c2 := soac.NewChanger()
+	c2.Bred().White().Underline()
+	fmt.Printf("%s is %s\n", c2.Apply("This"), c.Apply("combination!!"))
 }
