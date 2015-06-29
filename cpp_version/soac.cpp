60a2996aca60a99d2e76e889b78ab198960b891f
ami-GS <1991.daiki@gmail.com>
2015-06-29 22:57:49 +0900

    write detail implementation

--- cpp_version/soac.cpp
+++ cpp_version/soac.cpp
@@ -14,6 +14,139 @@ std::string Changer::Apply(int val) {
     return tmptmp;
 }
 
+
+Changer* Changer::black() {
+    fg = Black;
+    return this;
+}
+
+Changer* Changer::red() {
+    fg = Red;
+    return this;
+}
+
+
+Changer* Changer::green() {
+    fg = Green;
+    return this;
+}
+
+Changer* Changer::yellow() {
+    fg = Yellow;
+    return this;
+}
+
+Changer* Changer::blue() {
+    fg = Blue;
+    return this;
+}
+
+Changer* Changer::magenda() {
+    fg = Magenda;
+    return this;
+}
+
+Changer* Changer::cyan() {
+    fg = Cyan;
+    return this;
+}
+
+Changer* Changer::white() {
+    fg = White;
+    return this;
+}
+
+Changer* Changer::reset() {
+    attr = Reset;
+    return this;
+}
+
+Changer* Changer::bold() {
+    attr = Bold;
+    return this;
+}
+
+Changer* Changer::faint() {
+    attr = Faint;
+    return this;
+}
+
+Changer* Changer::italic() {
+    attr = Italic;
+    return this;
+}
+
+Changer* Changer::underline() {
+    attr = Underline;
+    return this;
+}
+
+Changer* Changer::blink1() {
+    attr = Blink1;
+    return this;
+}
+
+Changer* Changer::blink2() {
+    attr = Blink2;
+    return this;
+}
+
+Changer* Changer::reverse() {
+    attr = Reverse;
+    return this;
+}
+
+Changer* Changer::concealed() {
+    attr = Concealed;
+    return this;
+}
+
+Changer* Changer::crossedout() {
+    attr = Crossedout;
+    return this;
+}
+
+Changer* Changer::bgBlack() {
+    fg = BgBlack;
+    return this;
+}
+
+Changer* Changer::bgRed() {
+    fg = BgRed;
+    return this;
+}
+
+
+Changer* Changer::bgGreen() {
+    fg = BgGreen;
+    return this;
+}
+
+Changer* Changer::bgYellow() {
+    fg = BgYellow;
+    return this;
+}
+
+Changer* Changer::bgBlue() {
+    fg = BgBlue;
+    return this;
+}
+
+Changer* Changer::bgMagenda() {
+    fg = BgMagenda;
+    return this;
+}
+
+Changer* Changer::bgCyan() {
+    fg = BgCyan;
+    return this;
+}
+
+Changer* Changer::bgWhite() {
+    fg = BgWhite;
+    return this;
+}
+
 int main() {
     return 0;
 }
--- cpp_version/soac.h
+++ cpp_version/soac.h
@@ -52,4 +52,33 @@ public:
     Changer() : fg(39), attr(0), bg(49) {}
     std::string Apply(std::string str);
     std::string Apply(int val);
+
+    Changer* black();
+    Changer* red();
+    Changer* green();
+    Changer* yellow();
+    Changer* blue();
+    Changer* magenda();
+    Changer* cyan();
+    Changer* white();
+
+    Changer* reset();
+    Changer* bold();
+    Changer* faint();
+    Changer* italic();
+    Changer* underline();
+    Changer* blink1();
+    Changer* blink2();
+    Changer* reverse();
+    Changer* concealed();
+    Changer* crossedout();
+
+    Changer* bgBlack();
+    Changer* bgRed();
+    Changer* bgGreen();
+    Changer* bgYellow();
+    Changer* bgBlue();
+    Changer* bgMagenda();
+    Changer* bgCyan();
+    Changer* bgWhite();
 };
