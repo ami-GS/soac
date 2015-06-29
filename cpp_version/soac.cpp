67c622873383267b03f6e209dccd96a3b967bcfd
ami-GS <1991.daiki@gmail.com>
2015-06-29 22:57:06 +0900

    write implement in cpp file

--- cpp_version/soac.cpp
+++ cpp_version/soac.cpp
@@ -1,5 +1,19 @@
 #include "soac.h"
 
+std::string Changer::Apply(std::string val) {
+    tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m%v\x1b[0m";
+    std::string tmptmp = tmp.str();
+    tmp.str("");
+    return tmptmp;
+}
+
+std::string Changer::Apply(int val) {
+    tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m%v\x1b[0m";
+    std::string tmptmp = tmp.str();
+    tmp.str("");
+    return tmptmp;
+}
+
 int main() {
     return 0;
 }
--- cpp_version/soac.h
+++ cpp_version/soac.h
@@ -50,16 +50,6 @@ public:
     std::stringstream tmp;
 
     Changer() : fg(39), attr(0), bg(49) {}
-    std::string Apply(std::string str) {
-        tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m%v\x1b[0m";
-        std::string tmptmp = tmp.str();
-        tmp.str("");
-        return tmptmp;
-    }
-    std::string Apply(int val) {
-        tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m%v\x1b[0m";
-        std::string tmptmp = tmp.str();
-        tmp.str("");
-        return tmptmp;
-    }
+    std::string Apply(std::string str);
+    std::string Apply(int val);
 };
