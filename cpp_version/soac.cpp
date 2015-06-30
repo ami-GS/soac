a74d4faecd46a1aaf3e929c45f2225070454eae7
ami-GS <1991.daiki@gmail.com>
2015-06-30 14:51:17 +0900

    fix bug

--- cpp_version/soac.cpp
+++ cpp_version/soac.cpp
@@ -1,14 +1,14 @@
 #include "soac.h"
 
 std::string Changer::Apply(std::string val) {
-    tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m%v\x1b[0m";
+    tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m" << val << "\x1b[0m";
     std::string tmptmp = tmp.str();
     tmp.str("");
     return tmptmp;
 }
 
 std::string Changer::Apply(int val) {
-    tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m%v\x1b[0m";
+    tmp << "\x1b[" << attr << ";" << fg << ";" << bg << "m" << val << "\x1b[0m";
     std::string tmptmp = tmp.str();
     tmp.str("");
     return tmptmp;
