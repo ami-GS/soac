e93b31bc182b3bb2ae09f5a237d44b1386592132
ami-GS <1991.daiki@gmail.com>
2015-07-02 00:04:34 +0900

    update test

--- go/soac_test.go
+++ go/soac_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestNewChanger(t *testing.T) {
 	actual := NewChanger()
-	expected := &Changer{39, 0, 49}
+	expected := &Changer{39, 0, 49, false}
 
 	if actual.fg != expected.fg {
 		t.Errorf("got %v\nwant %v", actual.fg, expected.fg)
@@ -16,5 +16,8 @@ func TestNewChanger(t *testing.T) {
 	}
 	if actual.bg != expected.bg {
 		t.Errorf("got %v\nwant %v", actual.bg, expected.bg)
+	}
+	if actual.is256 != expected.is256 {
+		t.Errorf("got %v\nwant %v", actual.is256, expected.is256)
 	}
 }
